c829c0a1884a37dcebe37c14fd1bde609829a1ee
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:22:47 +0530

    improve

--- 2/admin/delete-doctor.php
+++ 2/admin/delete-doctor.php
@@ -1,28 +1,36 @@
 <?php
 
-    session_start();
+session_start();
 
-    if(isset($_SESSION["user"])){
-        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
-            header("location: ../login.php");
-        }
+// Redirect to login page if user is not logged in or not an admin
+if (!isset($_SESSION["user"]) || $_SESSION['usertype'] != 'a') {
+    header("location: ../login.php");
+    exit();
+}
 
-    }else{
-        header("location: ../login.php");
-    }
-    
-    
-    if($_GET){
-        //import database
-        include("../connection.php");
-        $id=$_GET["id"];
-        $result001= $database->query("select * from doctor where docid=$id;");
-        $email=($result001->fetch_assoc())["docemail"];
-        $sql= $database->query("delete from webuser where email='$email';");
-        $sql= $database->query("delete from doctor where docemail='$email';");
-        //print_r($email);
-        header("location: doctors.php");
-    }
+// Include database connection
+include("../connection.php");
 
+// Delete doctor if id is provided in GET request
+if (isset($_GET["id"]) && filter_var($_GET["id"], FILTER_VALIDATE_INT)) {
+    $id = $_GET["id"];
+    $emailQuery = $database->query("select docemail from doctor where docid=$id;");
+    $emailResult = $emailQuery->fetch_assoc();
+    $email = $emailResult["docemail"];
+
+    // Delete doctor from doctor table
+    $deleteDoctorQuery = $database->query("delete from doctor where docemail='$email';");
+
+    // Delete user from webuser table
+    $deleteUserQuery = $database->query("delete from webuser where email='$email';");
+
+    // Redirect to doctors page
+    header("location: doctors.php");
+    exit();
+}
+
+// Redirect to doctors page if no id is provided in GET request
+header("location: doctors.php");
+exit();
 
 ?>
