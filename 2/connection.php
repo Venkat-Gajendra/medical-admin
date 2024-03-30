622ed7180c2a2f830e87d1d45d8f43b11035dafa
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:22:43 +0530

    improve

--- 2/connection.php
+++ 2/connection.php
@@ -1,8 +1,14 @@
 <?php
 
-    $database= new mysqli("localhost:3307","root","","edoc");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
+const DB_HOST = 'localhost:3307';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'edoc';
+
+$database = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (@$database->connect_error) {
+    die('Connection failed: ' . @$database->connect_error);
+}
 
 ?>
