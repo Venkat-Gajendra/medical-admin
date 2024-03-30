7cabe92363cff29dbf8e7dce42f999d23d3d9200
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:24:00 +0530

    improve

--- resblog/blogadmin/adminview.php
+++ resblog/blogadmin/adminview.php
@@ -1,110 +1,122 @@
- <div class="row">
-          <div class="col-lg-3">
-            <div class="panel panel-warning">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col-xs-6">
-                    <i class="fa fa-desktop fa-5x"></i>
-                  </div>
-                  <div class="col-xs-6 text-right">
-                    <p class="announcement-heading"><?php admincounter("titles");?></p>
-                    <p class="announcement-text"><strong>Web Details!</strong></p>
-                  </div>
-                </div>
-              </div>
-              <a href="titles_view.php">
-                <div class="panel-footer announcement-bottom">
-                  <div class="row">
-                    <div class="col-xs-6">
-                      View
-                    </div>
-                    <div class="col-xs-6 text-right">
-                      <i class="fa fa-arrow-circle-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
+<div class="row">
+  <?php include 'admin_counters.php'; ?>
+  <?php include 'admin_links.php'; ?>
+  <?php include 'editors_choice.php'; ?>
+  <?php include 'admin_stats.php'; ?>
+</div><!-- /.row -->
+
+<!-- admin_counters.php -->
+<div class="col-lg-3">
+  <div class="panel panel-warning">
+    <div class="panel-heading">
+      <div class="row">
+        <div class="col-xs-6">
+          <i class="fa fa-desktop fa-5x"></i>
+        </div>
+        <div class="col-xs-6 text-right">
+          <p class="announcement-heading"><?= $webDetailsCount; ?></p>
+          <p class="announcement-text"><strong>Web Details!</strong></p>
+        </div>
+      </div>
+    </div>
+    <a href="titles_view.php" class="panel-footer-link">
+      <div class="panel-footer announcement-bottom">
+        <div class="row">
+          <div class="col-xs-6">
+            View
           </div>
-          <div class="col-lg-3">
-            <div class="panel panel-success">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col-xs-6">
-                    <i class="fa fa-link fa-5x"></i>
-                  </div>
-                  <div class="col-xs-6 text-right">
-                    <p class="announcement-heading"><?php admincounter("links");?></p>
-                    <p class="announcement-text"><strong>Links</strong></p>
-                  </div>
-                </div>
-              </div>
-              <a href="links_view.php">
-                <div class="panel-footer announcement-bottom">
-                  <div class="row">
-                    <div class="col-xs-6">
-                      View
-                    </div>
-                    <div class="col-xs-6 text-right">
-                      <i class="fa fa-arrow-circle-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
+          <div class="col-xs-6 text-right">
+            <i class="fa fa-arrow-circle-right"></i>
           </div>
-          <div class="col-lg-3">
-            <div class="panel panel-info">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col-xs-6">
-                    <i class="fa fa-trophy fa-5x"></i>
-                  </div>
-                  <div class="col-xs-6 text-right">
-                    <p class="announcement-heading"><?php admincounter("editors_choice");?></p>
-                    <p class="announcement-text"><strong>Editor's Choice</strong></p>
-                  </div>
-                </div>
-              </div>
-              <a href="editors_choice_view.php">
-                <div class="panel-footer announcement-bottom">
-                  <div class="row">
-                    <div class="col-xs-6">
-                      view
-                    </div>
-                    <div class="col-xs-6 text-right">
-                      <i class="fa fa-arrow-circle-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<!-- admin_links.php -->
+<div class="col-lg-3">
+  <div class="panel panel-success">
+    <div class="panel-heading">
+      <div class="row">
+        <div class="col-xs-6">
+          <i class="fa fa-link fa-5x"></i>
+        </div>
+        <div class="col-xs-6 text-right">
+          <p class="announcement-heading"><?= $linksCount; ?></p>
+          <p class="announcement-text"><strong>Links</strong></p>
+        </div>
+      </div>
+    </div>
+    <a href="links_view.php" class="panel-footer-link">
+      <div class="panel-footer announcement-bottom">
+        <div class="row">
+          <div class="col-xs-6">
+            View
           </div>
-          <div class="col-lg-3">
-            <div class="panel panel-primary">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col-xs-6">
-                    <i class="fa fa-bar-chart-o fa-5x"></i>
-                  </div>
-                  <div class="col-xs-6 text-right">
-                    <p class="announcement-heading"><?php adminstats("visitor_info","page_hits");?></p>
-                    <p class="announcement-text"><strong>Admin Stats</strong></p>
-                  </div>
-                </div>
-              </div>
-              <a href="../adminstats">
-                <div class="panel-footer announcement-bottom">
-                  <div class="row">
-                    <div class="col-xs-6">
-                      View
-                    </div>
-                    <div class="col-xs-6 text-right">
-                      <i class="fa fa-arrow-circle-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
+          <div class="col-xs-6 text-right">
+            <i class="fa fa-arrow-circle-right"></i>
           </div>
-        </div><!-- /.row -->
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<!-- editors_choice.php -->
+<div class="col-lg-3">
+  <div class="panel panel-info">
+    <div class="panel-heading">
+      <div class="row">
+        <div class="col-xs-6">
+          <i class="fa fa-trophy fa-5x"></i>
+        </div>
+        <div class="col-xs-6 text-right">
+          <p class="announcement-heading"><?= $editorsChoiceCount; ?></p>
+          <p class="announcement-text"><strong>Editor's Choice</strong></p>
+        </div>
+      </div>
+    </div>
+    <a href="editors_choice_view.php" class="panel-footer-link">
+      <div class="panel-footer announcement-bottom">
+        <div class="row">
+          <div class="col-xs-6">
+            view
+          </div>
+          <div class="col-xs-6 text-right">
+            <i class="fa fa-arrow-circle-right"></i>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<!-- admin_stats.php -->
+<div class="col-lg-3">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <div class="row">
+        <div class="col-xs-6">
+          <i class="fa fa-bar-chart-o fa-5x"></i>
+        </div>
+        <div class="col-xs-6 text-right">
+          <p class="announcement-heading"><?= $adminStatsCount; ?></p>
+          <p class="announcement-text"><strong>Admin Stats</strong></p>
+        </div>
+      </div>
+    </div>
+    <a href="../adminstats" class="panel-footer-link">
+      <div class="panel-footer announcement-bottom">
+        <div class="row">
+          <div class="col-xs-6">
+            View
+          </div>
+          <div class="col-xs-6 text-right">
+            <i class="fa fa-arrow-circle-right"></i>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
