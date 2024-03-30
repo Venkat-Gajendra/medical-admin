0254e8a33f15adca7d50a9d29675b103956324a4
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:24:15 +0530

    improve

--- resblog/blogadmin/resources/datepicker/js/lang/fr.js
+++ resblog/blogadmin/resources/datepicker/js/lang/fr.js
@@ -1,44 +1,61 @@
-var fdLocale = {
-        months:[
-                "Janvier",
-                "F\u00E9vrier",
-                "Mars",
-                "Avril",
-                "Mai",
-                "Juin",
-                "Juillet",
-                "Ao\u00FBt",
-                "Septembre",
-                "Octobre",
-                "Novembre",
-                "D\u00E9cembre"
-                ],
-        fullDay:[
-                "Lundi",
-                "Mardi",
-                "Mercredi",
-                "Jeudi",
-                "Vendredi",
-                "Samedi",
-                "Dimanche"
-                ],
-        
-        /* Only stipulate the dayAbbr should the first letter of the fullDay not suffice
-           
-        dayAbbr:[],
-        */
-        
-        /* Only stipulate the firstDayOfWeek should the first day not be Monday
-           
-        firstDayOfWeek:0,
-        */
-        
-        titles:[
-                "Mois pr\u00E9cedent",
-                "Mois suivant",
-                "Ann\u00E9e pr\u00E9cedente",
-                "Ann\u00E9e suivante",
-                "Aujourd\u2019hui",
-                "Voir Calendrier"
-                ]
-};
+const fdLocale = {
+  // Array of month names
+  months: [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"
+  ],
+
+  // Array of full day names
+  fullDay: [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche"
+  ],
+
+  // Array of abbreviated day names
+  dayAbbr: [
+    "Lun",
+    "Mar",
+    "Mer",
+    "Jeu",
+    "Ven",
+    "Sam",
+    "Dim"
+  ],
+
+  // Array of minimum day names (useful for small displays)
+  weekdaysMin: [
+    "L",
+    "Ma",
+    "Me",
+    "J",
+    "V",
+    "S",
+    "D"
+  ],
+
+  // Array of week titles
+  titles: [
+    "Mois précédent",
+    "Mois suivant",
+    "Année précédente",
+    "Année suivante",
+    "Aujourd'hui",
+    "Voir Calendrier"
+  ],
+
+  // The first day of the week (0: Sunday,
