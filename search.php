ef54908a7c75a5a46bf237de1deea6d84a9dfd69
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:24:37 +0530

    improve

--- search.php
+++ search.php
@@ -1,12 +1,18 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","myhmsdb");
-if(isset($_POST['search_submit'])){
-  $contact=$_POST['contact'];
-  $docname = $_SESSION['dname'];
- $query="select * from appointmenttb where contact='$contact' and doctor='$docname';";
- $result=mysqli_query($con,$query);
- echo '<!DOCTYPE html>
+$con = mysqli_connect("localhost", "root", "", "myhmsdb");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['search_submit'])) {
+    $contact = mysqli_real_escape_string($con, $_POST['contact']);
+    $docname = $_SESSION['dname'];
+    $query = "select * from appointmenttb where contact='$contact' and doctor='$docname';";
+    $result = mysqli_query($con, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo '<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -14,7 +20,7 @@ if(isset($_POST['search_submit'])){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body style="background-color:#342ac1;color:white;text-align:center;padding-top:50px;">
   <div class="container" style="text-align:left;">
@@ -32,31 +38,25 @@ if(isset($_POST['search_submit'])){
   </thead>
   <tbody>
   ';
-  while($row=mysqli_fetch_array($result)){
-    $fname=$row['fname'];
-    $lname=$row['lname'];
-    $email=$row['email'];
-    $contact=$row['contact'];
-    $appdate=$row['appdate'];
-    $apptime=$row['apptime'];
-    echo '<tr>
-      <td>'.$fname.'</td>
-      <td>'.$lname.'</td>
-      <td>'.$email.'</td>
-      <td>'.$contact.'</td>
-      <td>'.$appdate.'</td>
-      <td>'.$apptime.'</td>
+        while ($row = mysqli_fetch_array($result)) {
+            $fname = $row['fname'];
+            $lname = $row['lname'];
+            $email = $row['email'];
+            $contact = $row['contact'];
+            $appdate = $row['appdate'];
+            $apptime = $row['apptime'];
+            echo '<tr>
+      <td>' . $fname . '</td>
+      <td>' . $lname . '</td>
+      <td>' . $email . '</td>
+      <td>' . $contact . '</td>
+      <td>' . $appdate . '</td>
+      <td>' . $apptime . '</td>
     </tr>';
-  }
-echo '</tbody></table></div> 
+        }
+        echo '</tbody></table></div>
 <div><a href="doctor-panel.php" class="btn btn-light">Go Back</a></div>
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-  </body>
-</html>';
-}
-
-?>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/
