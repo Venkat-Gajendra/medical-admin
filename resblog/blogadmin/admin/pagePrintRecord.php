0083ac86478b9390551765adca78aaa949b9bf8c
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:24:17 +0530

    improve

--- resblog/blogadmin/admin/pagePrintRecord.php
+++ resblog/blogadmin/admin/pagePrintRecord.php
@@ -1,96 +1,58 @@
 <?php
-	$currDir = dirname(__FILE__);
-	require("{$currDir}/incCommon.php");
-	require("{$currDir}/incHeader.php");
-?>
+error_reporting(0);
 
-	<style>
-		nav{ display: none; }
-	</style>
+$currDir = dirname(__FILE__);
+include_once("{$currDir}/incCommon.php");
+include_once("{$currDir}/incHeader.php");
 
-<?php
+$response = [];
 
-	$recID = intval($_GET['recID']);
-	if(!$recID){
-		// no record provided
-		include("{$currDir}/incFooter.php");
-	}
+$recID = filter_var(intval($_GET['recID']), FILTER_SANITIZE_NUMBER_INT);
+if (empty($recID)) {
+    $response[] = Notification::show(array(
+        'message' => $Translation["record not found error"],
+        'class' => 'danger',
+        'dismiss_seconds' => 3600
+    ));
+    include("{$currDir}/incFooter.php");
+    exit;
+}
 
-	// fetch record data to fill in the form below
-	$res = sql("select * from membership_userrecords where recID='$recID'", $eo);
-	if(!($row = db_fetch_assoc($res))){
-		echo Notification::show(array(
-			'message' => $Translation["record not found error"],
-			'class' => 'danger',
-			'dismiss_seconds' => 3600
-		));
-		include("{$currDir}/incFooter.php");
-	}
+$res = sql("select * from membership_userrecords where recID='$recID'", $eo);
+if (empty($row = db_fetch_assoc($res))) {
+    $response[] = Notification::show(array(
+        'message' => $Translation["record not found error"],
+        'class' => 'danger',
+        'dismiss_seconds' => 3600
+    ));
+    include("{$currDir}/incFooter.php");
+    exit;
+}
 
-	// get record data
-	$tableName = $row['tableName'];
-	$pkValue = $row['pkValue'];
-	$memberID = strtolower($row['memberID']);
-	$dateAdded = @date($adminConfig['PHPDateTimeFormat'], $row['dateAdded']);
-	$dateUpdated = @date($adminConfig['PHPDateTimeFormat'], $row['dateUpdated']);
-	$groupID = $row['groupID'];
+extract($row);
 
-	// get pk field name
-	$pkField = getPKFieldName($tableName);
+$tableName = htmlentities($tableName, ENT_QUOTES, 'UTF-8');
+$pkValue = htmlentities($pkValue, ENT_QUOTES, 'UTF-8');
+$memberID = strtolower($memberID);
+$dateAdded = !empty($adminConfig['PHPDateTimeFormat']) ? date($adminConfig['PHPDateTimeFormat'], $dateAdded) : '';
+$dateUpdated = !empty($adminConfig['PHPDateTimeFormat']) ? date($adminConfig['PHPDateTimeFormat'], $dateUpdated) : '';
+$groupID = htmlentities($groupID, ENT_QUOTES, 'UTF-8');
 
-	$res = sql("select * from `{$tableName}` where `{$pkField}`='" . makeSafe($pkValue, false) . "'", $eo);
-	if(!($row = db_fetch_assoc($res))){
-		echo Notification::show(array(
-			'message' => $Translation["record not found error"],
-			'class' => 'danger',
-			'dismiss_seconds' => 3600
-		));
-		include("{$currDir}/incFooter.php");
-	}
+$pkField = getPKFieldName($tableName);
 
-	?>
-	<div class="page-header"><h1>
-		<?php echo str_replace("<TABLENAME>", $tableName, $Translation["table name"]); ?>
-	</h1></div>
+$res = sql("select * from `{$tableName}` where `{$pkField}`='" . makeSafe($pkValue, false) . "'", $eo);
+if (empty($row = db_fetch_assoc($res))) {
+    $response[] = Notification::show(array(
+        'message' => $Translation["record not found error"],
+        'class' => 'danger',
+        'dismiss_seconds' => 3600
+    ));
+    include("{$currDir}/incFooter.php");
+    exit;
+}
 
-	<table class="table table-striped table-bordered table-hover">
-		<thead>
-			<tr>
-				<th><?php echo $Translation["field name"]; ?></th>
-				<th><?php echo $Translation["value"]; ?></th>
-			</tr>
-		</thead>
+$thead = '<thead><tr><th>' . $Translation["field name"] . '</th><th>' . $Translation["value"] . '</th></tr></thead>';
+$tbody = '';
 
-		<tbody>
-		<?php
-			foreach($row as $fn => $fv){
-				$op = html_attr($fv);
-				if(@is_file("{$currDir}/../{$Translation['ImageFolder']}{$fv}")){
-					$op = "<a href=\"../{$Translation['ImageFolder']}{$fv}\" target=\"_blank\">" . html_attr($fv) . "</a>";
-				}
-
-				$tr_class = $pk_icon = '';
-				if($fn == $pkField){
-					$tr_class = 'class="text-primary"';
-					$pk_icon = '<i class="glyphicon glyphicon-star"></i> ';
-				}
-				?>
-				<tr <?php echo $tr_class; ?>>
-					<th valign="top"><?php echo $pk_icon . $fn; ?></th>
-					<td valign="top"><?php echo $op; ?></td>
-				</tr>
-				<?php
-			}
-		?>
-		</tbody>
-	</table>
-
-	<script>
-		$j(function(){
-			$j('nav').next('div').remove();
-		})
-	</script>
-
-<?php
-	include("{$currDir}/incFooter.php");
-?>
+foreach ($row as $fn => $fv) {
+    $fv = htmlspecialchars($fv, ENT_QUOTES, 'UTF-8');
