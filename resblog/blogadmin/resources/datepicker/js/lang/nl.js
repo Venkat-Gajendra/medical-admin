dc4b796cde42f6ed6a0d1aa4e2c2c6c18910486d
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:24:27 +0530

    improve

--- resblog/blogadmin/resources/datepicker/js/lang/nl.js
+++ resblog/blogadmin/resources/datepicker/js/lang/nl.js
@@ -1,40 +1,35 @@
 var fdLocale = {
-        months:[
-                "Januari",
-                "Februari",
-                "Maart",
-                "April",
-                "Mei",
-                "Juni",
-                "Juli",
-                "Augustus",
-                "September",
-                "Oktober",
-                "November",
-                "December"
-                ],
-        fullDay:[
-                "Maandag",
-                "Dinsdag",
-                "Woensdag",
-                "Donderdag",
-                "Vrijdag",
-                "Zaterdag",
-                "Zondag"
-                ],
-        /* Only stipulate the dayAbbr should the first letter of the fullDay not suffice
-
-        dayAbbr:[],
-        */
-
-        /* Only stipulate the firstDayOfWeek should the first day not be Monday
-
-        firstDayOfWeek:0,
-        */
-        titles:[
-                "Vorige Maand",
-                "Volgende Maand",
-                "Vorig jaar",
-                "Volgend jaar"
-                ]
+    months:[
+            "Januari",
+            "Februari",
+            "Maart",
+            "April",
+            "Mei",
+            "Juni",
+            "Juli",
+            "Augustus",
+            "September",
+            "Oktober",
+            "November",
+            "December"
+            ],
+    fullDay:[
+            "Maandag",
+            "Dinsdag",
+            "Woensdag",
+            "Donderdag",
+            "Vrijdag",
+            "Zaterdag",
+            "Zondag"
+            ],
+    dayAbbr: ['Ma', 'Di', ' Wo', 'Do', 'Vr', 'Za', 'Zo'],
+    firstDayOfWeek: 1, // 0 for Sunday, 1 for Monday
+    titles:[
+            "Vorige Maand",
+            "Volgende Maand",
+            "Vorig jaar",
+            "Volgend jaar"
+            ],
+    dateFormat: 'dd-mm-yyyy', // 'dd' for day, 'mm' for month, 'yyyy' for year
+    firstWeekContainsDate: 1 // The first week of the year that contains at least this date is considered week 1
 };
