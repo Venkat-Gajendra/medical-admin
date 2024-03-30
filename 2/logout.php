be2355a74bf6ea94554a52e87c1a9dd9737ba5f6
Venkat Gajendra <98881078+Venkat-Gajendra@users.noreply.github.com>
2024-03-30 21:23:02 +0530

    improve

--- 2/logout.php
+++ 2/logout.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
 
-	session_start();
+// Start the session and destroy all session variables
+session_start();
+session_destroy();
 
-	$_SESSION = array();
+// Unset all session cookies
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+}
 
-	if (isset($_COOKIE[session_name()])) {
-		setcookie(session_name(), '', time()-86400, '/');
-	}
+// Redirect the user to the login page
+header('Location: login.php?action=logout');
+exit;
 
-	session_destroy();
-
-	// redirecting the user to the login page
-	header('Location: login.php?action=logout');
-
- ?>
+?>
